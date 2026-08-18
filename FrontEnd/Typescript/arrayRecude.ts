137a1ff2dd3148100b943bee169f2a5c985c8f6d
akshay.dave <akshaydave311@gmail.com>
2026-08-19 01:17:55 +0530

    [LEARN] daily prep

---
+++ FrontEnd/Typescript/arrayRecude.ts
@@ -0,0 +1,13 @@
+const arr = [ 1, 2, 8, 14 ]
+const sum = arr.reduce((a, b) => a + b, 0);
+
+console.log(sum); // 25 --> 0 + 1 = 1, 1 + 2 = 3, 3 + 8 = 11, 11 + 14 = 25
+
+
+const newArr = [['Potatoes', 3], ['Tomatoes', 85], ['Onions', 27]];
+const result = newArr.reduce((a: any, b: any) => {
+    a[b[0]] = b[1]
+    return a;
+}, {});
+
+console.log(result);
